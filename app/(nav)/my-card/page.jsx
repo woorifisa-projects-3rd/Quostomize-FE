6bc995afe89bbd2d1af2e7dc0d74e2a1f5452b0d
seonmin5 <seonmin5555@gmail.com>
2024-11-19 12:33:37 +0900

    Feat/PieceStock: 조각투자토글 와이어프레임

--- app/(nav)/my-card/page.jsx
+++ app/(nav)/my-card/page.jsx
@@ -1,9 +1,17 @@
+import Image from 'next/image';
+import MyToggle from '../../../components/button/toggle'
+
 const MyCardPage = () => {
-    return (
+  return (
+    <div>
       <div>
-        내 카드 페이지
+        {/* TODO: API 통신 및 s3 이미지 주소로 변경 필요 */}
+        <h3>조각투자</h3>
+        <MyToggle />
+        <Image src='' alter='조각투자 이미지' width={50} height={50}></Image>
       </div>
-    );
-  }
-  
-  export default MyCardPage;
+    </div>
+  );
+}
+
+export default MyCardPage;
