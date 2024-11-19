959b7d2bd0f27401bfddfb066a8289134e3e638b
bangsk <cats7858@gmail.com>
2024-11-19 20:31:20 +0900

    Refactor/home Homebody4, HomeFoot CSS 적용 HomeBody4 아이콘 적용

--- app/(nav)/home/page.jsx
+++ app/(nav)/home/page.jsx
@@ -5,22 +5,22 @@ import HomeBody1 from '../../../components/home/home-body1';
 import HomeBody2 from '../../../components/home/home-body2';
 import HomeBody3 from '../../../components/home/home-body3';
 import HomeBody4 from '../../../components/home/home-body4';
-import HomeFoot from '../../../components/home/home-foot';
+
 
 const Home = () => {
   return (
 
     <div className={`h-full overflow-scroll [&::-webkit-scrollbar]:hidden`}
-      style={{
-        scrollbarWidth: 'none',
-        msOverflowStyle: 'none',
-      }}>
+    style={{
+    scrollbarWidth: 'none',
+     msOverflowStyle: 'none',
+    }}>
       <HomeHeader /><br />
       <HomeBody1 /><br />
       <HomeBody2 /><br />
       <HomeBody3 /><br />
       <HomeBody4 /><br />
-      <HomeFoot />
+      
     </div>
   );
 }
