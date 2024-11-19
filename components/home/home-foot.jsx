1b84a8f8f74b4047b29fd044d6508be734705097
bangsk <cats7858@gmail.com>
2024-11-19 17:47:11 +0900

    Feat/homefoot : 카드만들기 버튼 생성

---
+++ components/home/home-foot.jsx
@@ -0,0 +1,14 @@
+"use client";
+
+import MyFullButton from '../button/full-button'
+
+const HomeFoot = () => {
+
+    return (
+        <div>
+            <MyFullButton href=''>카드 만들기</MyFullButton>
+        </div>
+    );
+}
+
+export default HomeFoot;
