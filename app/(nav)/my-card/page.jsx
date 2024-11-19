8ce98ad178aa0d78e5810b13dac1a8c0e70c10c9
seonmin5 <seonmin5555@gmail.com>
2024-11-19 12:34:29 +0900

    Feat/PieceStock: 조각투자토글 와이어프레임

--- app/(nav)/my-card/page.jsx
+++ app/(nav)/my-card/page.jsx
@@ -4,8 +4,8 @@ import MyToggle from '../../../components/button/toggle'
 const MyCardPage = () => {
   return (
     <div>
+      {/* TODO: API 통신 및 s3 이미지 주소로 변경 필요 */}
       <div>
-        {/* TODO: API 통신 및 s3 이미지 주소로 변경 필요 */}
         <h3>조각투자</h3>
         <MyToggle />
         <Image src='' alter='조각투자 이미지' width={50} height={50}></Image>
