8b3be227b18203ed0ea46c80a5d900846aaef373
seonmin5 <seonmin5555@gmail.com>
2024-11-19 14:22:32 +0900

    Feat/Payback: 페이백 와이어프레임

--- app/(nav)/my-card/page.jsx
+++ app/(nav)/my-card/page.jsx
@@ -6,9 +6,9 @@ const MyCardPage = () => {
     <div>
       {/* TODO: API 통신 및 s3 이미지 주소로 변경 필요 */}
       <div>
-        <h3>일일복권</h3>
+        <h3>페이백</h3>
         <MyToggle />
-        <Image src='' alter='일일복권 이미지' width={50} height={50}></Image>
+        <p>포인트의 80% 페이백 받기</p>
       </div>
     </div>
   );
