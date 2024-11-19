994c70ab27e36dd7842a610506baab7a9dcbdb9e
seonmin5 <seonmin5555@gmail.com>
2024-11-19 14:12:44 +0900

    Feat/Lotto: 일일복권토글 와이어프레임

--- app/(nav)/my-card/page.jsx
+++ app/(nav)/my-card/page.jsx
@@ -6,9 +6,9 @@ const MyCardPage = () => {
     <div>
       {/* TODO: API 통신 및 s3 이미지 주소로 변경 필요 */}
       <div>
-        <h3>조각투자</h3>
+        <h3>일일복권</h3>
         <MyToggle />
-        <Image src='' alter='조각투자 이미지' width={50} height={50}></Image>
+        <Image src='' alter='일일복권 이미지' width={50} height={50}></Image>
       </div>
     </div>
   );
