f569032ade25b6f8194830e6d9822ef9abc287e5
bangsk <cats7858@gmail.com>
2024-11-19 14:40:34 +0900

    HomeBody1 기본 구조 구현

--- app/(nav)/home/page.jsx
+++ app/(nav)/home/page.jsx
@@ -1,13 +1,15 @@
 "use client";
 
 import HomeHeader from './home-header';
+import HomeBody1 from './home-body1'
 
 const Home = () => {
   return (
 
     <div>
       <HomeHeader />
-      
+      <HomeBody1 />
+
     </div>
   );
 }
