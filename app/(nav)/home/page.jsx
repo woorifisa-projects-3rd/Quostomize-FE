8de2e16e5bee5c4ac413e4777a09170db8d76f41
bangsk <cats7858@gmail.com>
2024-11-19 16:10:45 +0900

    Refactor/home components 분리

--- app/(nav)/home/page.jsx
+++ app/(nav)/home/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
-import HomeHeader from './home-header';
-import HomeBody1 from './home-body1';
+import HomeHeader from '../../../components/home/home-header';
+import HomeBody1 from '../../../components/home/home-body1';
 import HomeBody2 from './home-body2';
 
 const Home = () => {
