774031b8f8427895e9a1def4ba1a5e1033084187
seonmin5 <seonmin5555@gmail.com>
2024-11-21 02:55:46 +0900

    Refactor/CardBenefitDetails: benefit-details-body2 내용 및 css 수정

--- components/cardBenefitDetails/benefit-details-body2.jsx
+++ components/cardBenefitDetails/benefit-details-body2.jsx
@@ -7,27 +7,27 @@ const BenefitDetailsBody2 = () => {
             iconSrc: 'https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Activities/Wrapped%20Gift.png',
             category: '쇼핑',
             descriptions: [
-                '백화점(더현대, 신세계, 롯데백화점)',
-                '온라인 쇼핑(무신사, 에이블리)',
-                '마트(이마트, 홈플러스)'
+                '백화점 - 더현대, 신세계, 롯데백화점',
+                '온라인 쇼핑 - 무신사, 에이블리',
+                '마트 - 이마트, 홈플러스'
             ]
         },
         {
             iconSrc: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Automobile.png",
             category: '생활',
             descriptions: [
-                '주유소(SK, GS칼텍스)',
-                '통신(SKT, KT, LGU+)',
-                '대중교통(버스, 지하철, 택시)'
+                '주유소 - SK, GS칼텍스',
+                '통신 - SKT, KT, LGU+',
+                '대중교통 - 버스, 지하철, 택시'
             ]
         },
         {
             iconSrc: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Food/Hamburger.png",
             category: '푸드',
             descriptions: [
-                '편의점(CU, GS25)',
-                '카페(스타벅스, 투썸플레이스)',
-                '배달(배달의 민족, 쿠팡이츠)'
+                '편의점 - CU, GS25',
+                '카페 - 스타벅스, 투썸플레이스',
+                '배달 - 배달의 민족, 쿠팡이츠'
             ]
         },
         {
@@ -35,26 +35,26 @@ const BenefitDetailsBody2 = () => {
             iconSrc: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Travel%20and%20places/Airplane.png",
             category: '여행',
             descriptions: [
-                '투어(인터파크투어, 마이리얼트립)',
-                '렌트(쏘카, 그린카)',
-                '숙소(야놀자, 에어비앤비)'
+                '투어 - 인터파크투어, 마이리얼트립',
+                '렌트 - 쏘카, 그린카',
+                '숙소 - 야놀자, 에어비앤비'
             ]
         },
         {
             iconSrc: "https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Objects/Mirror%20Ball.png",
             category: '문화',
             descriptions: [
-                'OTT(넷플릭스, 티빙)',
-                '영화(CGV, 롯데시네마)',
-                '도서(밀리의 서재, 교보문고)'
+                'OTT - 넷플릭스, 티빙',
+                '영화 - CGV, 롯데시네마',
+                '도서 - 밀리의 서재, 교보문고'
             ]
         },
     ];
 
     return (
-        <div className="p-6">
-            <h1 className="text-xl font-bold mb-2">혜택 적용 브랜드</h1>
-            <p className="text-gray-600 mb-6">나만의 브랜드를 선택하면 1% 추가로 적립 받을 수 있어요</p>
+        <div className="mt-10">
+            <h1 className="font3 font-bold mb-2">혜택 받는 가맹점</h1>
+            <p className="mb-10 font1">가맹점 카테고리를 선택하고 1% 추가 적립 받으세요</p>
             {brands.map((brand, index) => (
                 <BrandBox
                     key={index}
