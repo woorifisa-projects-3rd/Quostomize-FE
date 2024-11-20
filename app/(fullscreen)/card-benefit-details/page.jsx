ad5891e6c826c360bf19a9ccf973289ddc392621
bangsk <cats7858@gmail.com>
2024-11-20 18:40:27 +0900

    Feat/benefitDetail benefitDetailfooter2 카드 만들기 버튼 구현

--- app/(fullscreen)/card-benefit-details/page.jsx
+++ app/(fullscreen)/card-benefit-details/page.jsx
@@ -1,5 +1,6 @@
 import BenefitDetailsHeader from "../../../components/cardBenefitDetails/benefit-details-header";
 import PointUsageBody from "../../../components/cardBenefitDetails/point-usage-body";
+import BenefitFooter2 from "../../../components/cardBenefitDetails/benefit-details-footer2";
 
 const CardDetailsPage = () => {
   return (
@@ -7,6 +8,7 @@ const CardDetailsPage = () => {
       <strong>카드 혜택 상세 페이지입니다.</strong>
       <BenefitDetailsHeader />
       <PointUsageBody />
+      <BenefitFooter2 />
 
     </div>
   );
