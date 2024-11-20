35a928fe5b1af32bb3698ccfe79ca120453e82e5
bangsk <cats7858@gmail.com>
2024-11-20 18:28:39 +0900

    Feat/benefitdetail tab-content-box component 구현 및 PointUsageBody 구현

--- app/(fullscreen)/card-benefit-details/page.jsx
+++ app/(fullscreen)/card-benefit-details/page.jsx
@@ -1,10 +1,13 @@
 import BenefitDetailsHeader from "../../../components/cardBenefitDetails/benefit-details-header";
+import PointUsageBody from "../../../components/cardBenefitDetails/point-usage-body";
 
 const CardDetailsPage = () => {
   return (
     <div>
       <strong>카드 혜택 상세 페이지입니다.</strong>
       <BenefitDetailsHeader />
+      <PointUsageBody />
+
     </div>
   );
 }
