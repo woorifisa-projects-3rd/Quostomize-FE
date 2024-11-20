656efbb6a756d6b0f69fbb634927a7e6bc9faab1
seonmin5 <seonmin5555@gmail.com>
2024-11-20 17:31:06 +0900

    Feat/benefitDetailsGraph: 움직이는 혜택 그래프 추가

--- app/(fullscreen)/card-benefit-details/page.jsx
+++ app/(fullscreen)/card-benefit-details/page.jsx
@@ -1,10 +1,15 @@
-import BenefitDetailsHeader from "../../../components/cardBenefitDetails/benefit-details-header";
+import BenefitDetailsHeader from '../../../components/cardBenefitDetails/benefit-details-header'
+import BenefitChartV1 from '../../../components/cardBenefitDetails/benefit-graph-v1'
+import BenefitChartV2 from '../../../components/cardBenefitDetails/benefit-graph-v2'
+
 
 const CardDetailsPage = () => {
   return (
-    <div>
+    <div className='grid place-items-center h-screen'>
       <strong>카드 혜택 상세 페이지입니다.</strong>
       <BenefitDetailsHeader />
+      <BenefitChartV1 />
+      {/* <BenefitChartV2 /> */}
     </div>
   );
 }
