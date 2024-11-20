10bcc3f11267b10e9ddb3209abfc6cd0b1a43baa
seonmin5 <seonmin5555@gmail.com>
2024-11-20 11:28:06 +0900

    Feat/benefitDetailsHeader: 카드혜택상세 페이지 생성

---
+++ app/(fullscreen)/card-benefit-details/page.jsx
@@ -0,0 +1,12 @@
+import BenefitDetailsHeader from "../../../components/cardBenefitDetails/benefit-details-header";
+
+const CardDetailsPage = () => {
+  return (
+    <div>
+      <strong>카드 혜택 상세 페이지입니다.</strong>
+      <BenefitDetailsHeader />
+    </div>
+  );
+}
+
+export default CardDetailsPage;
