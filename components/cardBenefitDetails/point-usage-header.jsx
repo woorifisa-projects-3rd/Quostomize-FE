853c7bd666e4b4016cd9d3c5c0eb650523df82cc
seonmin5 <seonmin5555@gmail.com>
2024-11-21 02:58:58 +0900

    Refactor/CardBenefitDetails: point-usage-header 내용 및 css 수정

--- components/cardBenefitDetails/point-usage-header.jsx
+++ components/cardBenefitDetails/point-usage-header.jsx
@@ -4,11 +4,9 @@ import React from 'react'
 
 function PointUsageHeader() {
     return (
-        <div className='flex flex-col justify-center items-center mt-20'>
-            <h1 className='font4 font-semibold'>세 가지 중 원하는</h1>
-            <h1 className='font4 font-semibold'>
-                <span className='color1'>포인트</span>를 직접 골라요</h1>
-
+        <div className='flex flex-col justify-center items-center mt-20 mb-10 font4 font-bold'>
+            <h1>재미있는 <span className='color1'>포인트 사용처</span></h1>
+            <h1>원할 때 <span className='color1'>켜고 끄세요</span></h1>
         </div>
     )
 }
