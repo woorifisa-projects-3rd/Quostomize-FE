eec49aa36b543caa5a5e9f33e5acbe53a1b0f480
seonmin5 <seonmin5555@gmail.com>
2024-11-21 02:59:31 +0900

    Feat/CardBenefitDetails: back-button 컴포넌트 추가

---
+++ components/button/back-button.jsx
@@ -0,0 +1,22 @@
+'use client'
+
+import { useRouter } from 'next/navigation';
+
+const BackButton = ({ children }) => {
+    const router = useRouter();
+
+    const handleBack = () => {
+        router.back();  // 이전 페이지로 이동
+    };
+
+    return (
+        <button
+            onClick={handleBack}
+            className="absolute top-4 left-4 p-2 bg-blue-500 text-white rounded-full shadow-lg hover:bg-blue-700 transition font1 pl-4 pr-4"
+        >
+            &#8592; {children}
+        </button>
+    );
+};
+
+export default BackButton;
