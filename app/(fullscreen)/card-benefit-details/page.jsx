b7f44f1e9cae91a32aceb78f7e1a2dc3e8ff3639
seonmin5 <seonmin5555@gmail.com>
2024-11-20 23:51:26 +0900

    Feat/benefitDetailsBody1: 혜택 상세 안내 페이지 추가

--- app/(fullscreen)/card-benefit-details/page.jsx
+++ app/(fullscreen)/card-benefit-details/page.jsx
@@ -1,29 +1,34 @@
 import BenefitDetailsHeader from "../../../components/cardBenefitDetails/benefit-details-header";
-import PointUsageHeader from "../../../components/cardBenefitDetails/point-usage-header";
-import PointUsageBody from "../../../components/cardBenefitDetails/point-usage-body";
-import BenefitFooter1 from "../../../components/cardBenefitDetails/benefit-details-footer1";
-import BenefitFooter2 from "../../../components/cardBenefitDetails/benefit-details-footer2";
 
-import BenefitDetailsHeader from '../../../components/cardBenefitDetails/benefit-details-header'
 import BenefitChartV1 from '../../../components/cardBenefitDetails/benefit-graph-v1'
 import BenefitChartV1ColorChange from '../../../components/cardBenefitDetails/benefit-graph-v1-colorchange'
 import BenefitChartV2 from '../../../components/cardBenefitDetails/benefit-graph-v2'
+
+import BenefitDetailsBody1 from '../../../components/cardBenefitDetails/benefit-body1'
 import BenefitDetailsBody2 from '../../../components/cardBenefitDetails/benefit-body2'
+
+import PointUsageHeader from "../../../components/cardBenefitDetails/point-usage-header";
+import PointUsageBody from "../../../components/cardBenefitDetails/point-usage-body";
+
+import BenefitFooter1 from "../../../components/cardBenefitDetails/benefit-details-footer1";
+import BenefitFooter2 from "../../../components/cardBenefitDetails/benefit-details-footer2";
 
 const CardDetailsPage = () => {
   return (
     <div className='grid place-items-center h-screen'>
-      <strong>카드 혜택 상세 페이지입니다.</strong>
       <BenefitDetailsHeader />
-
-      <PointUsageHeader />
-      <PointUsageBody />
-      <BenefitFooter1 />
-      <BenefitFooter2 />
       <BenefitChartV1 />
       <BenefitChartV1ColorChange />
       {/* <BenefitChartV2 /> */}
+
+      <BenefitDetailsBody1 />
       <BenefitDetailsBody2 />
+
+      <PointUsageHeader />
+      <PointUsageBody />
+
+      <BenefitFooter1 />
+      <BenefitFooter2 />
     </div>
   );
 }
