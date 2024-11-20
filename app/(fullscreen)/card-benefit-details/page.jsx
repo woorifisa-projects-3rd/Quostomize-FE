32c51fad8891b785906118f026074cc56bd22be3
bangsk <cats7858@gmail.com>
2024-11-20 18:56:45 +0900

    Feat/benefit benefitDetailFooter1 유의사항 토글 박스 구현

--- app/(fullscreen)/card-benefit-details/page.jsx
+++ app/(fullscreen)/card-benefit-details/page.jsx
@@ -1,5 +1,6 @@
 import BenefitDetailsHeader from "../../../components/cardBenefitDetails/benefit-details-header";
 import PointUsageBody from "../../../components/cardBenefitDetails/point-usage-body";
+import BenefitFooter1 from "../../../components/cardBenefitDetails/benefit-details-footer1";
 import BenefitFooter2 from "../../../components/cardBenefitDetails/benefit-details-footer2";
 
 const CardDetailsPage = () => {
@@ -8,6 +9,7 @@ const CardDetailsPage = () => {
       <strong>카드 혜택 상세 페이지입니다.</strong>
       <BenefitDetailsHeader />
       <PointUsageBody />
+      <BenefitFooter1 />
       <BenefitFooter2 />
 
     </div>
