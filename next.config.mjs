cc233cde0fb5ab786177c6218cb34581984a2caf
newgamer11 <qfpotw4u81@naver.com>
2024-11-21 14:00:28 +0900

    Feat/displayMyStock:  next.config.mjs s3 이미지 설정 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "quostomize-bucket.s3.ap-northeast-2.amazonaws.com",
+                port: "",
+                pathname: "/image/*",
+            },
+        ],
+    },
+};
 
 export default nextConfig;
