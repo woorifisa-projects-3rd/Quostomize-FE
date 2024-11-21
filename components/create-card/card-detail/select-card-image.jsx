f2b375ce1270eb7ce6642e9477efe9792066c2cc
seonmin5 <seonmin5555@gmail.com>
2024-11-22 00:22:58 +0900

    Feat/selectDesign1: SelectCardImage 수정 및 뒤집기 애니메이션 추가

--- components/create-card/card-detail/select-card-image.jsx
+++ components/create-card/card-detail/select-card-image.jsx
@@ -2,43 +2,23 @@
 
 import React, { useState } from 'react'
 import Image from 'next/image'
+import FlipCard from '../../../components/card/flip-card'
 
 function SelectCardImage() {
-    const tempCard = {
-        id: 5,
-        name: 'yellow',
-        image: '/cards-images/yellow-image.png'
-    };
+    // 임시
+    const i = 1;
 
     return (
-        <div className="flex flex-col w-full bg-gray-50">
+        <div>
             {/* 제목 영역 */}
             <div className="px-6 pt-6 mb-4">
-                <h2 className="text-2xl font-bold text-left text-gray-800">
-                    카드정보를 <span className="text-blue-400">선택 </span>해주세요
+                <h2 className="font3 font-bold text-left">
+                    카드정보를 <span className="color1">선택</span>해 주세요
                 </h2>
             </div>
-            
-            {/* 카드 섹션 */}
-            <div className="w-full space-y-2">
-                {/* 커스터마이징 카드 텍스트 */}
-                <p className="text-xl font-extrabold text-lime-500 text-center">
-                    커스터마이징 카드
-                </p>
-                
-                {/* 카드 이미지 */}
-                <div className="w-full bg-gray-50">
-                    <div className="flex justify-center">
-                        <Image
-                            src={tempCard.image}
-                            alt="Selected card"
-                            width={200}
-                            height={360}
-                            className="transform -rotate-90"
-                            priority
-                        />
-                    </div>
-                </div>
+            {/* 카드 이미지 */}
+            <div className="flex justify-center">
+                <FlipCard frontImg={`/cards-images/${i}f.png`} backImg={`/cards-images/${i}b.png`} />
             </div>
         </div>
     )
