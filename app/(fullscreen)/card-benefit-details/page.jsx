39d9bcbf8c2d1c4a6239c63fa5eaf68baa8f700e
seonmin5 <seonmin5555@gmail.com>
2024-11-21 11:04:44 +0900

    Remove/CardBenefitDetails: 사용하지 않는 혜택 그래프 페이지에서 삭제

--- app/(fullscreen)/card-benefit-details/page.jsx
+++ app/(fullscreen)/card-benefit-details/page.jsx
@@ -3,7 +3,6 @@ import BackButton from '../../../components/button/back-button'
 import BenefitDetailsHeader from "../../../components/cardBenefitDetails/benefit-details-header";
 import BenefitChartV1 from '../../../components/cardBenefitDetails/benefit-graph-v1'
 import BenefitChartV1ColorChange from '../../../components/cardBenefitDetails/benefit-graph-v1-colorchange'
-import BenefitChartV2 from '../../../components/cardBenefitDetails/benefit-graph-v2'
 import BenefitDetailsBody1 from '../../../components/cardBenefitDetails/benefit-details-body1'
 import BenefitDetailsBody2 from '../../../components/cardBenefitDetails/benefit-details-body2'
 import PointUsageHeader from "../../../components/cardBenefitDetails/point-usage-header";
@@ -18,9 +17,8 @@ const CardDetailsPage = ({ children }) => {
 
       <div className='grid place-items-center'>
         <BenefitDetailsHeader />
-        <BenefitChartV1 />
+        {/* <BenefitChartV1 /> */}
         <BenefitChartV1ColorChange />
-        {/* <BenefitChartV2 /> */}
 
         <BenefitDetailsBody1 />
         <BenefitDetailsBody2 />
