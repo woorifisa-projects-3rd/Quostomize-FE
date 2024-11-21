c1e1426dfe3429de27c9e1d65f57e6a2429a18f2
newgamer11 <qfpotw4u81@naver.com>
2024-11-21 10:33:10 +0900

    Feat/StockAutoRecommendClickChoiceComplete: 기존개수 와 추천개수 합을 고려했을때 3개 초과시 예외모달 생성

--- app/piece-stock/favorite/page.jsx
+++ app/piece-stock/favorite/page.jsx
@@ -3,6 +3,7 @@
 import Image from "Next/image"
 import { useEffect, useState } from "react";
 import LargeModal from "../../../components/overlay/largeModal";
+import RecommendAlertmodal from "../../../components/piece-stock/favorite/recommendAlertmodal"
 
 const FavoritePage = () => {
   const [wishInfo, setWishInfo] = useState([]); // 조회한 위쉬 정보를 저장
@@ -12,20 +13,17 @@ const FavoritePage = () => {
   const [hoveredIndex, setHoveredIndex] = useState([{ order: 0 }, { order: 0 }, { order: 0 }]); // Hover된 항목의 인덱스를 관리
   const [isOpen, setOpen] = useState(false)
   const [isClickButton, setClickButton] = useState([false, false, false])
+  const [showAlertModal, setShowAlertModal] = useState(false);
 
   useEffect(() => {
     searchWishStocks();
   }, [])
 
-  useEffect(() => {
-    console.log(recommendStockInfo)
-  }, [recommendStockInfo])
-
   const param = new URLSearchParams();
 
-  const cardId = 2
+  const cardId = 1
   //예시value
-  param.append("cardId", 2)
+  param.append("cardId", cardId)
 
   // 백엔드에서 GET 위시리스트 조회시, 위시리스트의 priority, stockName, stockPresentPrice, stockImage 를 갖고온다.
   const searchWishStocks = async () => {
@@ -223,7 +221,17 @@ const FavoritePage = () => {
       newData[index] = !newData[index];
       return newData;
     });
-    console.log("확인요" + isClickButton[index])
+  }
+
+  const choiceStocks = () => {
+    console.log(wishInfo.length)
+    console.log(isClickButton.filter(item => item === true).length)
+    if ((wishInfo.length + isClickButton.filter(item => item === true).length) > 3) {
+      setShowAlertModal(true)
+      setTimeout(() => {
+        setShowAlertModal(false); // 1초 후에 모달 숨기기
+      }, 2000); //
+    }
   }
 
   return (
@@ -282,51 +290,56 @@ const FavoritePage = () => {
           <button className="px-4 py-2 border rounded border-black font-bold" onClick={openModal}>종목 자동 추천</button>
           <button className="px-4 py-2 border rounded border-black font-bold" onClick={switchStock}>저장</button>
         </div>
-        {isOpen && <LargeModal className="bg-white rounded-t-3xl p-6 w-full"
-          title={
-            <div className="text-xl font-bold text-center mb-2">
-              선택한 카드 혜택 내역을 기반으로 추천해요
-            </div>
-          }
-          description={
-            <div className="text-sm text-center mb-6">
-              카드 혜택을 기반으로 3종목을 추천해요.
-              원하는 만큼 선택해서 반영해보세요.
-            </div>
-          }
-          onClose={closeModal}
-          setIsOpen={closeModal}
-          choice={"선택완료"}
-          cancle={"닫기"}
-        >
-          {recommendStockInfo.map((stock, index) => (
-            <div key={stock.stockName} className="flex justify-between items-center p-4">
-              <div className={`flex justify-between w-full `}>
-                <div className="flex">
-                  <div className="w-12 h-12 rounded-full overflow-hidden">
-                    <Image src={stock.stockImage} width={50} height={50} alt="주식이미지"></Image>
+        <div>
+          {isOpen && <LargeModal className="bg-white rounded-t-3xl p-6 w-full"
+            title={
+              <p className="text-xl font-bold text-center mb-2">
+                선택한 카드 혜택 내역을 기반으로 추천해요
+              </p>
+            }
+            description={
+              <p className="text-sm text-center mb-6">
+                카드 혜택을 기반으로 3종목을 추천해요.원하는 만큼 선택해서 반영해보세요.
+              </p>
+            }
+            onClose={choiceStocks}
+            setIsOpen={closeModal}
+            choice={"선택완료"}
+            cancle={"닫기"}
+          >
+            {recommendStockInfo.map((stock, index) => (
+              <div key={stock.stockName} className="flex justify-between items-center p-4">
+                <div className={`flex justify-between w-full `}>
+                  <div className="flex">
+                    <div className="w-12 h-12 rounded-full overflow-hidden">
+                      <Image src={stock.stockImage} width={50} height={50} alt="주식이미지"></Image>
+                    </div>
+                    <div className="flex h-full items-center ml-4">
+                      <p className="font-medium">{stock.stockName}</p>
+                    </div>
                   </div>
-                  <div className="flex h-full items-center ml-4">
-                    <p className="font-medium">{stock.stockName}</p>
-                  </div>
-                </div>
-                <div className="flex items-center">
-                  <p className="text-gray-600 text-sm">{stock.stockPresentPrice}원</p>
-                  <div className={`w-6 h-6 rounded-full border-2 flex items-center justify-center cursor-pointer
+                  <div className="flex items-center">
+                    <p className="text-gray-600 text-sm">{stock.stockPresentPrice}원</p>
+                    <div className={`w-6 h-6 rounded-full border-2 flex items-center justify-center cursor-pointer
           ${isClickButton[index]
-                      ? 'border-blue-500 bg-blue-500'
-                      : 'border-gray-300 bg-gray-100'
-                    }`}
-                    onClick={() => checkButton(index)}>
-                    {isClickButton[index] && (
-                      <span className="text-white text-sm">✓</span>
-                    )}
+                        ? 'border-blue-500 bg-blue-500'
+                        : 'border-gray-300 bg-gray-100'
+                      }`}
+                      onClick={() => checkButton(index)}>
+                      {isClickButton[index] && (
+                        <span className="text-white text-sm">✓</span>
+                      )}
+                    </div>
                   </div>
                 </div>
+                {showAlertModal && <RecommendAlertmodal
+                  title={"이미 3개가 선택되어있어요 삭제하고 선택해주세요."}
+                >
+                </RecommendAlertmodal>}
               </div>
-            </div>
-          ))}
-        </LargeModal>}
+            ))}
+          </LargeModal>}
+        </div>
       </ul>
     </>
   )
