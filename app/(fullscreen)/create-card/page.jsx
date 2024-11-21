408e4c779415d94625ee67cdf77705518b7aca66
bangsk <cats7858@gmail.com>
2024-11-21 17:20:41 +0900

    Feat/selectPoint3 다음페이지로 이동 버튼 구현

--- app/(fullscreen)/create-card/page.jsx
+++ app/(fullscreen)/create-card/page.jsx
@@ -2,15 +2,16 @@
 
 import SelectPoint1 from '../../../components/create-card/select-point1';
 import SelectPoint2 from '../../../components/create-card/select-point2';
+import SelectPoint3 from '../../../components/create-card/select-point3';
 
 const CreateCardPage = () => {
   return (
     <div>
       <header>
         <SelectPoint1 />
-        <SelectPoint2 />
       </header>
-
+      <SelectPoint2 />
+      <SelectPoint3 />
     </div>
   );
 }
