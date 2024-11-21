476b3476afcde93bfb5524282f23469631b20d8f
bangsk <cats7858@gmail.com>
2024-11-21 19:39:33 +0900

    Refactor/cardrefactor 카드생성 부분 tab 형식으로 화면전환을 위한 기본 구조 프레임 구현

--- components/create-card/select-point/select-point1.jsx
+++ components/create-card/select-point/select-point1.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
-import Header from "../header/prev-name-exit";
+import Header from "../../header/prev-name-exit";
 
-function SelectPoint1() {
 
-  const handleArrowClick = () => {
-    window.history.back();
-  };
+function SelectPoint1({ onClick }) {
+  console.log("onClick received:", onClick);
 
   return (
 
     <div>
       <Header
         showArrowButton={true}
-        onArrowClick={handleArrowClick}
+        onArrowClick={onClick}
         exitDirection="/home"
       >
         포인트 사용 옵션 선택
