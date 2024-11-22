ef3c601621b60473f78b5b1fd0c4a7ba051fb7fd
seonmin5 <seonmin5555@gmail.com>
2024-11-22 11:24:44 +0900

    Feat/selectDesign1: create crad 페이지에 SelectDesign1 추가

--- app/(fullscreen)/create-card/page.jsx
+++ app/(fullscreen)/create-card/page.jsx
@@ -3,9 +3,9 @@
 import SelectPoint1 from '../../../components/create-card/select-point/select-point1';
 import SelectPoint2 from '../../../components/create-card/select-point/select-point2';
 import CreateCardBottom from '../../../components/create-card/create-card-bottom';
-import CarouselCard from '../../../components/card/carousel-card'
 import SelectCardImage from '../../../components/create-card/card-detail/select-card-image';
 import SelectCardDetail from '../../../components/create-card/card-detail/select-card-detail';
+import SelectDesign1 from '../../../components/create-card/select-design/select-design1'
 
 import React, { useState } from "react";
 
@@ -42,7 +42,7 @@ const CreateCardPage = () => {
           <header>
             <SelectPoint1 onClick={handlePrevPage} />
           </header>
-          <CarouselCard />
+          <SelectDesign1 />
         </div>;
 
       case 2:
