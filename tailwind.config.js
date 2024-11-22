7d78712959258f1d29ee9c418401be90282bfb4f
seonmin5 <seonmin5555@gmail.com>
2024-11-22 11:22:20 +0900

    Design/selectDesign1: tailwind color 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,11 @@ module.exports = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        purple: '#ed3ef7',
+        black: '#000000',
+        red: '#ff0000',
+        green: '#bcdc33',
+        milk: '#dbdbdb',
       },
     },
   },
