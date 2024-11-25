5744de8727d627e79cb323848bb05b1684db475d
newgamer11 <qfpotw4u81@naver.com>
2024-11-25 16:53:27 +0900

    Refactor/SearchStockClickChoiceComplete: piece-stock/search/page.jsx -  stockInformation 에서 조회하고 선택할 떄, 기존에 이미 위시리스트에 등록된 주식종목에 대한 예외처리 코드 작성 완료

--- components/piece-stock/search/bottom.jsx
+++ components/piece-stock/search/bottom.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import RecommendAlertmodal from "../../../components/piece-stock/etc/recommendAlertModal"
 import { useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 const SearchBottom = ({ selectedStocks, cardId, session }) => {
     const [isAlert, setAlert] = useState(false);
-    const [wishInfo, setWishInfo] = useState([])
+    const [wishInfo, setWishInfo] = useState([]);
+    const FavoriteData = []
+    const saveData = []
 
     const param = new URLSearchParams();
 
-    // 백엔드에서 GET 위시리스트 조회시, 위시리스트의 priority, stockName, stockPresentPrice, stockImage 를 갖고온다.
+    const router = useRouter()
+
+
     const searchWishStocks = async () => {
         param.append("cardId", cardId)
         try {
@@ -31,16 +36,78 @@ const SearchBottom = ({ selectedStocks, cardId, session }) => {
         }
     }
 
-    // 
+    // 저장 요청
+    const saveStocks = async () => {
+        try {
+            const response = await fetch(`http://localhost:8080/v1/api/stocks/recommendations/add?${param}`, {
+                method: 'GET',
+                credentials: 'include',
+                headers: {
+                    'Content-Type': 'application/json',  // 요청 본문이 JSON임을 지정
+                    'Authorization': `Bearer ${session.accessToken}`, // JWT 토큰을 Authorization 헤더에 포함
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error('값이 조회되지 않았습니다.');
+            }
+        } catch (error) {
+            console.error('데이터 가져오기 오류:', error);
+        }
+    }
+
+    // 중복 확인로직
     const check = () => {
         searchWishStocks()
         if ((wishInfo.length + selectedStocks.length) > 3) {
             setAlert(true)
             setTimeout(() => {
                 setAlert(false)
-            }, 1000);
+            }, 1500);
+        } else {
+            selectedStocks.forEach((checkStockName, i) => { // 선택된 주식을 저장하는 코드
+                const testName = []
+                let duplicated = []
+                let check = false;
+                const sample = checkStockName
+                wishInfo.forEach((whishStock, index) => { // 내 위시주식 정보에서
+                    if (whishStock.stockName === checkStockName) { // 만일 위시주식 안에 추천주식명이 없다면 통과
+                        console.log("중복이 존재한다 : " + index)
+                        duplicated.push(true) // 중복일떄 true를 넣는다.
+                    } else {    //중복이 있다면 체크
+                        console.log("주식의 중복이 없다면 : " + index)
+                        console.log(whishStock.stockName)
+                        console.log(checkStockName)
+                        console.log("===============")
+                        duplicated.push(false) // 중복이 아닐떄 false를 넣는다.
+                    }
+                })
+                check = true;
+                testName.push(sample)
+
+                if (duplicated.includes(true)) {
+                    console.log("위시주식에 이미 해당 주식이 존재합니다.");
+                } else {
+                    if (check === true) {
+                        testName.forEach((stockName) => {
+
+                            param.append("stockName", stockName)
+                            console.log(param)
+                            saveStocks(); // 저장하는 함수 호출 (주석 처리된 상태)
+                            console.log(stockName)
+                            console.log("위시주식 저장");
+                            param.delete("stockName")
+                        })
+                    } else {
+                        console.log("다음 추천주식 인덱스로")
+                    }
+
+                }
+            })
+            router.push("/piece-stock/favorite")
         }
     }
+
 
 
 
