8525d4e0114766a16d338954938e7c2895d50f7e
Kee0304 <kee0304@gmail.com>
2024-11-25 15:42:11 +0900

    Feat/MainPage: 과거 당첨자 확인 페이지 구현

--- components/button/square-button.jsx
+++ components/button/square-button.jsx
@@ -1,9 +1,9 @@
 import Link from "next/link"
 
-const SquareButton = async ({href, message}) => {
+const SquareButton = async ({href, message, as}) => {
 
   return (
-    <Link href={href}>
+    <Link href={href} as={as}>
       <div className="w-28 h-10 leading-10 color1 text-center align-middle rounded-md">
         {message}
       </div>
