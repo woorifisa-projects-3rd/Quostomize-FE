cbd8041a915f9f8392ccfa311a38893bb49104e8
bangsk <cats7858@gmail.com>
2024-11-25 15:45:26 +0900

    Feat/ChangeBenefit3 혜택 변경: tab 형식 혜택 선택 박스 구현

--- app/(nav)/change-benefits/page.jsx
+++ app/(nav)/change-benefits/page.jsx
@@ -2,6 +2,7 @@
 
 import ChangeBenefitHeader from "../../../components/change-benefits/ChangeBenefitHeader";
 import ChangeBenefitBody1 from "../../../components/change-benefits/ChangeBenefitBody1";
+import ChangeBenefitBody2 from "../../../components/change-benefits/ChangeBenefitBody2";
 import { BenefitProvider } from "../../../components/create-card/select-benefit/BenefitContext";
 
 const ChangeBenefitsPage = () => {
@@ -10,8 +11,8 @@ const ChangeBenefitsPage = () => {
       <ChangeBenefitHeader />
       <BenefitProvider>
         <ChangeBenefitBody1 />
+        <ChangeBenefitBody2 />
       </BenefitProvider>
-
 
 
     </div>
