24001753d3f25d7201cd6f99484ce057e2351696
newgamer11 <qfpotw4u81@naver.com>
2024-11-27 13:32:25 +0900

    Feat/Stock: piece-stock/favorite - route.js 생성

--- app/api/piece-stock/favorite/route.js
+++ app/api/piece-stock/favorite/route.js
@@ -1,13 +1,13 @@
 // 
 import { NextResponse } from "next/server";
-import { auth } from "../../../auth";
+import { auth } from "../../../../auth";
 
 
-export async function GET({ param }) {
-    // 서버 엔드포인트 지정
+export async function GET0() { // 카드조회
+
     const session = await auth();
 
-    const response = await fetch(`${process.env.SERVER_URL}/v1/api/stocks/select?${param}`,
+    const response = await fetch(`${process.env.SERVER_URL}/v1/api/benefit-change`, // 서버 엔드포인트 지정
         {
             method: "GET",
             headers: {
@@ -17,8 +17,69 @@ export async function GET({ param }) {
             credentials: "include",
             cache: "no-store"
         }
-
     );
+
+    if (!response.ok) {
+        throw new Error('데이터를 가져오지 못했습니다.');
+    }
+
+    const result = await response.json();
+
+    if (response.status >= 400) {
+        return NextResponse.json({ redirectUrl: 'login' }, { status: response.status })
+    } else {
+        return NextResponse.json(result.data, { status: 200 });
+    }
+};
+
+export async function GET1(param) { // 위시조회
+
+    const session = await auth();
+
+    const response = await fetch(`${process.env.SERVER_URL}/v1/api/stocks/select?${param}`, // 서버 엔드포인트 지정
+        {
+            method: "GET",
+            headers: {
+                "Content-type": "application",
+                "Authorization": `Bearer ${session.accessToken}`
+            },
+            credentials: "include",
+            cache: "no-store"
+        }
+    );
+
+    if (!response.ok) {
+        throw new Error('데이터를 가져오지 못했습니다.');
+    }
+
+    const result = await response.json();
+
+    if (response.status >= 400) {
+        return NextResponse.json({ redirectUrl: 'login' }, { status: response.status })
+    } else {
+        return NextResponse.json(result.data, { status: 200 });
+    }
+};
+
+export async function GET2(param) { // 추천종목조회
+
+    const session = await auth();
+
+    const response = await fetch(`${process.env.SERVER_URL}/v1/api/stocks/recommendations?${param}`, // 서버 엔드포인트 지정
+        {
+            method: "GET",
+            headers: {
+                "Content-type": "application",
+                "Authorization": `Bearer ${session.accessToken}`
+            },
+            credentials: "include",
+            cache: "no-store"
+        }
+    );
+
+    if (!response.ok) {
+        throw new Error('데이터를 가져오지 못했습니다.');
+    }
 
     const result = await response.json();
 
@@ -26,5 +87,102 @@ export async function GET({ param }) {
         return NextResponse.json({ redirectUrl: 'login' }, { status: response.status })
     } else {
         return NextResponse.json(result, { status: 200 });
+    }
+};
+
+export async function GET3(param) { // 종목 추가
+
+    const session = await auth();
+
+    const response = await fetch(`${process.env.SERVER_URL}/v1/api/stocks/recommendations/add?${param}`, // 서버 엔드포인트 지정
+        {
+            method: "GET",
+            headers: {
+                "Content-type": "application",
+                "Authorization": `Bearer ${session.accessToken}`
+            },
+            credentials: "include",
+            cache: "no-store"
+        }
+    );
+
+    if (!response.ok) {
+        throw new Error('데이터를 가져오지 못했습니다.');
+    }
+
+    const result = await response.json();
+
+    if (response.status >= 400) {
+        return NextResponse.json({ redirectUrl: 'login' }, { status: response.status })
+    } else {
+        return NextResponse.json(result, { status: 200 });
+    }
+};
+
+export async function PATCH(orderInfo) { // 위시조회
+
+    const session = await auth();
+
+    const response = await fetch(`${process.env.SERVER_URL}/v1/api/stocks/select/change-rank`, // 서버 엔드포인트 지정
+        {
+            method: "PATCH",
+            headers: {
+                "Content-type": "application",
+                "Authorization": `Bearer ${session.accessToken}`
+            },
+            body: JSON.stringify(orderInfo),
+            credentials: "include",
+            cache: "no-store"
+        }
+    );
+
+    if (!response.ok) {
+        throw new Error('데이터를 가져오지 못했습니다.');
+    }
+
+    const result = await response.json();
+
+    if (response.status >= 400) {
+        return NextResponse.json({ redirectUrl: 'login' }, { status: response.status })
+    }
+    if (!result.status === 204) {
+        const errorData = await result.json();
+        return NextResponse.json(
+            { message: errorData.message || '서버 오류 발생', status: result.status }
+        );
+    }
+};
+
+
+export async function DELETE(param) { // 위시삭제
+
+    const session = await auth();
+
+    const response = await fetch(`${process.env.SERVER_URL}/v1/api/stocks/select?${param}`, // 서버 엔드포인트 지정
+        {
+            method: "DELETE",
+            headers: {
+                "Content-type": "application",
+                "Authorization": `Bearer ${session.accessToken}`
+            },
+            credentials: "include",
+            cache: "no-store"
+        }
+    );
+
+    if (!response.ok) {
+        throw new Error('데이터를 가져오지 못했습니다.');
+    }
+
+    const result = await response.json();
+
+    if (response.status >= 400) {
+        return NextResponse.json({ redirectUrl: 'login' }, { status: response.status })
+    }
+    if (!result.status === 204) {
+        const errorData = await result.json();
+        return NextResponse.json(
+            { message: errorData.message || '서버 오류 발생', status: result.status }
+        );
     }
 };
