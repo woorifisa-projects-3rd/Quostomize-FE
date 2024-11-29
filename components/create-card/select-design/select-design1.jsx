c1a36d3569b6437284ed70219f301129d60fb0b1
kimh7537 <kimh7537@gmail.com>
2024-11-29 14:47:08 +0900

    Refactor/createCard: 1페이지 useState상위로 변경

--- components/create-card/select-design/select-design1.jsx
+++ components/create-card/select-design/select-design1.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 import CarouselCard from '../../card/carousel-card'
 
-const SelectDesign1 = () => {
+const SelectDesign1 = ({ selectedCardIndex, onCardChange }) => {
 
     const colorInfo = [
         {
@@ -43,39 +43,39 @@ const SelectDesign1 = () => {
         }
     ];
 
-    const [currentColorIndex, setCurrentColorIndex] = useState(0);
-
-    const handleCardChange = (index) => {
-        setCurrentColorIndex(index);
-    };
-
     return (
-        <div className='flex flex-col items-center gap-4 font-bold font4'>
-            <CarouselCard onCardChange={handleCardChange} />
-            <span>내가 선택한 {" "}
+        <div className="flex flex-col items-center gap-4 font-bold font4">
+            {/* onCardChange를 CarouselCard에 그대로 전달 */}
+            <CarouselCard onCardChange={onCardChange}/>
+            <span>
+                내가 선택한{' '}
                 <span
-
-                    className={`text-transparent bg-clip-text
-                        animate-gradient-text
-                        bg-[length:400%_400%]`}
-                    style={colorInfo[currentColorIndex].style}
+                    className="text-transparent bg-clip-text animate-gradient-text bg-[length:400%_400%]"
+                    style={colorInfo[selectedCardIndex].style}
                 >
-                    {colorInfo[currentColorIndex].name}</span>
+                    {colorInfo[selectedCardIndex].name}
+                </span>
             </span>
 
             <style jsx global>{`
                 @keyframes gradient-text {
-                    0% {background-position: 0% 50%;}
-                    50% {background-position: 100% 50%;}
-                    100% {background-position: 0% 50%;}
+                    0% {
+                        background-position: 0% 50%;
+                    }
+                    50% {
+                        background-position: 100% 50%;
+                    }
+                    100% {
+                        background-position: 0% 50%;
+                    }
                 }
+
                 .animate-gradient-text {
                     animation: gradient-text 3s ease infinite;
                 }
-            `}
-            </style>
-        </div >
-    )
-}
+            `}</style>
+        </div>
+    );
+};
 
 export default SelectDesign1
