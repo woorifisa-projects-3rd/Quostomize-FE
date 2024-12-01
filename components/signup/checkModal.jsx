04a433830912e8c15bb1d44991c61fa8ff11d67c
newgamer11 <qfpotw4u81@naver.com>
2024-12-02 00:51:52 +0900

    Refactor/Signup - singup/page.jsx -피드백 을 반영한  페이지 컴퍼넌트 분리

--- components/signup/checkModal.jsx
+++ components/signup/checkModal.jsx
@@ -1,8 +1,12 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
+import ModalHeader from "./modalComponent/ModalHeader"
+import ModalData from "./modalComponent/modalData"
+import ChevronButton from "./modalComponent/chevronButton"
 
-const CheckModal = ({ onClose, onComplete, isConfirm, setNumber, number1 }) => {
+
+const CheckModal = ({ onClose, setNumber, number1 }) => {
     const [shuffledNumbers, setShuffledNumbers] = useState([]);
     const MAX_LENGTH = 6;
 
@@ -40,54 +44,16 @@ const CheckModal = ({ onClose, onComplete, isConfirm, setNumber, number1 }) => {
     return (
         <div className="absolute inset-0 flex justify-center items-center z-50">
             <div className="w-[576px] bg-[#192436] h-full flex flex-col items-center">
-                <button
-                    onClick={onClose}
-                    className="self-start text-white mt-4 ml-4"
-                >
-                    ←
-                </button>
-
-                <h3 className="text-white text-lg mt-20 mb-6">
-                    {isConfirm}
-                </h3>
-
-                <div className="flex justify-center space-x-3 mb-20">
-                    {Array(MAX_LENGTH)
-                        .fill(0)
-                        .map((_, idx) => (
-                            <div
-                                key={idx}
-                                className={`w-2 h-2 rounded-full ${idx < number1?.length ? 'bg-gray-300' : 'bg-gray-600'
-                                    }`}
-                            />
-                        ))}
-                </div>
-
-                <div className="mt-40 grid grid-cols-3 gap-16 text-center text-white w-full max-w-[400px]">
-                    {shuffledNumbers.slice(0, 9).map((number, index) => (
-                        <button
-                            key={`button-${number}`}
-                            onClick={() => handleKeyPress(number)}
-                            className="text-2xl"
-                        >
-                            {number}
-                        </button>
+                <ChevronButton onClick={()=>onClose()} classNameProps={`self-start text-white mt-4 ml-4`}>←</ChevronButton>
+                <h3 className="text-white text-lg mt-20 mb-6"/>
+                   <ModalHeader maxLength={MAX_LENGTH} number1={number1}></ModalHeader>
+                <div className="mt-40 grid grid-cols-3 gap-16 text-center text-white w-full max-w-[400px]" >
+                    {shuffledNumbers.slice(0, 9).map((number,index) => (
+                        <ModalData number={number} onClick={()=> handleKeyPress(number)}></ModalData>
                     ))}
                     <div key="empty-space" />
-                    <button
-                        key={`button-${shuffledNumbers[9]}`}
-                        onClick={() => handleKeyPress(shuffledNumbers[9])}
-                        className="text-2xl"
-                    >
-                        {shuffledNumbers[9]}
-                    </button>
-                    <button
-                        key="delete-button"
-                        onClick={() => handleDelete()}
-                        className="text-2xl"
-                    >
-                        ←
-                    </button>
+                    <ChevronButton onClick={()=>handleKeyPress(shuffleNumbers[9])} classNameProps={`text-2xl`}>{shuffledNumbers[9]}</ChevronButton>
+                    <ChevronButton onClick={()=>handleDelete()} classNameProps={`text-2xl`}>←</ChevronButton>
                 </div>
             </div>
         </div>
