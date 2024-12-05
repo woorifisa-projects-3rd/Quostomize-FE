3711798c13fe27849d84219f695962779c02bf0a
Kee0304 <kee0304@gmail.com>
2024-12-05 23:49:18 +0900

    HOTFIX/build: 빌드용 파일 정리

--- components/signup/checkModal.jsx
+++ components/signup/checkModal.jsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-import ModalHeader from "./modalComponent/ModalHeader"
+import ModalHeader from "./modalComponent/modalHeader"
 import ModalData from "./modalComponent/modalData"
 import ChevronButton from "./modalComponent/chevronButton"
 
