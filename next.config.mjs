ce07dd2cf996c8015c9220a0997d983946e80927
ChanUi <hcu55@naver.com>
2024-12-06 22:10:49 +0900

    Refactor/config: next.config.mjs 수정

--- next.config.mjs
+++ next.config.mjs
@@ -9,40 +9,36 @@ const nextConfig = {
                 port: "",
                 pathname: "/image/*",
             },
+            {
+                protocol: 'https',
+                hostname: 'raw.githubusercontent.com',
+                pathname: '/Tarikul-Islam-Anik/Animated-Fluent-Emojis/**'
+            }
         ],
-        domains: ["raw.githubusercontent.com"]
+        domains: ["quostomize-bucket.s3.ap-northeast-2.amazonaws.com", "raw.githubusercontent.com"]
     },
 
     webpack: config => {
         config.module.rules.push({
-          test: /\.svg$/,
-          use: ["@svgr/webpack"],
+            test: /\.svg$/,
+            use: ["@svgr/webpack"],
         });
-    
+
         return config;
-      },
+    },
 
     experimental: {
         turbo: {
             rules: {
-            '*.svg': {
-                loaders: ['@svgr/webpack'],
-                as: '*.js',
+                '*.svg': {
+                    loaders: ['@svgr/webpack'],
+                    as: '*.js',
                 },
             },
         },
     },
 
-    images: {
-        remotePatterns: [
-            {
-            protocol: 'https',
-            hostname: 'raw.githubusercontent.com',
-            pathname: '/Tarikul-Islam-Anik/Animated-Fluent-Emojis/**'
-            }
-        ]
-    }, 
-    
     reactStrictMode: false,
 }
+
 export default nextConfig;
