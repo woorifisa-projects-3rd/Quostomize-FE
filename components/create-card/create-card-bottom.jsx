64d677ea9459bc2ae154b24c2d3db42baa9b5bac
ChanUi <hcu55@naver.com>
2024-12-06 22:14:43 +0900

    Refactor/createCard: 카드 생성 다음 버튼 UI 수정

--- components/create-card/create-card-bottom.jsx
+++ components/create-card/create-card-bottom.jsx
@@ -8,9 +8,9 @@ function CreateCardBottom({ onClick, currentPage, totalPage }) {
 
 
     return (
-        <div className='flex flex-col p-4 justify-center items-center'>
+        <div className='flex flex-col p-9 justify-center items-center'>
             <Button onClick={onClick}
-                className={`font-bold font2 w-full rounded bg-blue-600 py-3 px-3 text-white ${isLastPage ? "data-[hover]:bg-green-500 data-[active]:bg-green-700" : "data-[hover]:bg-blue-500 data-[active]:bg-sky-700"}
+                className={`font-bold font2 w-full rounded bg-blue-400 py-3 px-3 text-white ${isLastPage ? "data-[hover]:bg-green-500 data-[active]:bg-green-700" : "data-[hover]:bg-blue-500 data-[active]:bg-sky-600"}
                 }`}>
                 {isLastPage ? "완 료" : "다 음"}
             </Button>
