2e5ee62ef81dbaf373e9bf2317900afc0cbcffa1
seonmin5 <seonmin5555@gmail.com>
2024-12-08 03:41:18 +0900

    Design/publishing: created card bottom 다음 버튼 클릭시 최상단 포지셔닝

--- components/create-card/create-card-bottom.jsx
+++ components/create-card/create-card-bottom.jsx
@@ -5,11 +5,14 @@ import { Button } from '@headlessui/react'
 
 function CreateCardBottom({ onClick, currentPage, totalPage }) {
     const isLastPage = currentPage === totalPage;
-
+    const handleClick = () => {
+        onClick();
+        window.scrollTo(0, 0);
+    };
 
     return (
         <div className='flex flex-col px-4 py-2 justify-center items-center'>
-            <Button onClick={onClick}
+            <Button onClick={handleClick}
                 className={`font-bold w-full rounded bg-blue-400 py-3 px-3 text-white ${isLastPage ? "data-[hover]:bg-green-300 data-[active]:bg-green-500" : "data-[hover]:bg-blue-500 data-[active]:bg-sky-600"}
                 }`}>
                 {isLastPage ? "신 청" : "다 음"}
