3c5cb61c31ddd1785eab8fc2bccfc51b8eef57d4
ChanUi <hcu55@naver.com>
2024-12-08 13:35:11 +0900

    Refator/alertModal: 카드 생성 페이지 입력 알림 모달 퍼블리싱

--- app/(fullscreen)/create-card/AlertModal.jsx
+++ app/(fullscreen)/create-card/AlertModal.jsx
@@ -9,21 +9,21 @@ const AlertModal = ({ isOpen, onClose, message }) => {
         <div className="fixed inset-0 z-50 flex items-center justify-center">
             {/* Backdrop */}
             <div
-                className="absolute inset-0 bg-black/40 backdrop-blur-sm"
+                className="absolute inset-0 bg-black/50 backdrop-blur-none"
                 onClick={onClose}
             />
 
             {/* Modal Content */}
-            <div className="relative bg-white rounded-2xl shadow-xl w-[90%] max-w-md transform transition-all">
-                <div className="p-8 pt-10">
+            <div className="relative bg-white rounded-2xl shadow-xl w-[80%] max-w-xs transform transition-all">
+                <div className="px-6 pt-8 pb-5 mx-1">
                     {/* Icon and Message with better centering */}
-                    <div className="flex items-center justify-center pl-2 mb-4">
+                    <div className="flex items-center justify-center mb-3">
                         <img
                             src={Icons.exclamation}
                             alt="경고"
-                            className="w-10 h-10 animate-bounce mr-4"
+                            className="w-9 h-9 an mr-3"
                         />
-                        <p className="text-lg font-bold text-gray-800">
+                        <p className="text-lg font-bold text-gray-700">
                             {message}
                         </p>
                     </div>
@@ -31,10 +31,10 @@ const AlertModal = ({ isOpen, onClose, message }) => {
                     {/* Button */}
                     <button
                         onClick={onClose}
-                        className="w-full mt-8 px-4 py-3 bg-blue-500 text-white rounded-xl
-                                 font-semibold hover:bg-blue-600 transition-colors
-                                 active:scale-[0.98] transform duration-100
-                                 shadow-md hover:shadow-lg"
+                        className="w-full mt-5 px-4 py-2 sm:py-2 bg-blue-400 text-white rounded-xl
+                                font-semibold hover:bg-blue-500 transition-colors
+                                active:scale-[0.98] transform duration-100 text-lg
+                                shadow-md hover:shadow-lg"
                     >
                         확인
                     </button>
