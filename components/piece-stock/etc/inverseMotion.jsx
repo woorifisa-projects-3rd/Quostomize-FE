58d52fc3fbefc654d3ac60b8eac274b27612dc4d
newgamer11 <qfpotw4u81@naver.com>
2024-12-08 19:16:55 +0900

    Refactor/Stock: 삭제버튼 색깔 반영 안되는 문제 코드 수정

--- components/piece-stock/etc/inverseMotion.jsx
+++ components/piece-stock/etc/inverseMotion.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const inverseMotion = ({ hoveredIndex, index, onClick, children }) => {
     return (
-        <div className={`absolute right-0 bg-red h-full top-0 rounded-lg text-[#FFFFFF] font-semibold px-4 transform  transition-all duration-500 ease-in-out 
+        <div className={`absolute right-0 bg-red-500 h-full top-0 rounded-lg text-[#FFFFFF] font-semibold px-4 transform  transition-all duration-500 ease-in-out 
 flex items-center justify-center text-center" ${hoveredIndex[index]?.order === 1 ? 'translate-x-0 opacity-100 bg-[#B8B8B8] w-20' : 'translate-x-[100%] opacity-0'}`}
             onClick={onClick}>
             {children}
