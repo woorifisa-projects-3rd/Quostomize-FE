5dea2dd194beedca88d2768b009640b9c7d341d7
seonmin5 <seonmin5555@gmail.com>
2024-12-08 15:27:42 +0900

    Design/publishing: benefit details footer2 flex 삭제

--- components/cardBenefitDetails/benefit-details-footer2.jsx
+++ components/cardBenefitDetails/benefit-details-footer2.jsx
@@ -6,7 +6,7 @@ import MyFullButton from '../button/full-button'
 function BenefitFooter2() {
 
     return (
-        <div className='flex flex-col justify-center items-center mt-10 mb-20'>
+        <div className='flex-col justify-center items-center mt-10 mb-20'>
             <MyFullButton href='/create-card'>카드 만들기</MyFullButton>
         </div>
     )
